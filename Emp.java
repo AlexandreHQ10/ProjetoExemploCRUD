b084ec1e9a4e6ee419580044b3ca4c06b5de8c27
Alexandre Henrique <88166141+AlexandreHQ10@users.noreply.github.com>
2023-10-27 21:47:58 -0300

    Update Emp.java

--- Emp.java
+++ Emp.java
@@ -1,48 +1,45 @@
-<!DOCTYPE html>
-<html>
+public class Emp {
+	private int id;
+	private String name, password, email, country;
 
-<head>
-	<meta charset="ISO-8859-1">
-	<title>Insert title here</title>
-</head>
+	public int getId() {
+		return id;
+	}
 
-<body>
+	public void setId(int id) {
+		this.id = id;
+	}
 
-	<h1>Add New Employee</h1>
-	<form action="SaveServlet1" method="post">
-		<table>
-			<tr>
-				<td>Name:</td>
-				<td><input type="text" name="name" /></td>
-			</tr>
-			<tr>
-				<td>Password:</td>
-				<td><input type="password" name="password" /></td>
-			</tr>
-			<tr>
-				<td>Email:</td>
-				<td><input type="email" name="email" /></td>
-			</tr>
-			<tr>
-				<td>Country:</td>
-				<td>
-					<select name="country" style="width:150px">
-						<option>India</option>
-						<option>USA</option>
-						<option>UK</option>
-						<option>Other</option>
-					</select>
-				</td>
-			</tr>
-			<tr>
-				<td colspan="2"><input type="submit" value="Save Employee" /></td>
-			</tr>
-		</table>
-	</form>
+	public String getName() {
+		return name;
+	}
 
-	<br />
-	<a href="ViewServlet">view employees</a>
+	public void setName(String name) {
+		this.name = name;
+	}
 
-</body>
+	public String getPassword() {
+		return password;
+	}
 
-</html>
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
